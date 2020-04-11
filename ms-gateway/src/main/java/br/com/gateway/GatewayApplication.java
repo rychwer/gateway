c092d56cd56db6cd00d4ca8db6cbaf72154becdc
Arthur Alves <arthur.alvesdeveloper@outlook.com>
2020-04-10 23:27:21 -0300

    DockerFile e DockerComposer

--- ms-gateway/src/main/java/br/com/gateway/GatewayApplication.java
+++ ms-gateway/src/main/java/br/com/gateway/GatewayApplication.java
@@ -10,7 +10,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class GatewayApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
